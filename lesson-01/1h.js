95b3053a52ecec8abdf0b2a4fa1f9a7c53489bd9
muhammadullah <muhammad.swat881@gmail.com>
2025-12-27 10:27:24 +0500

    Lesson-01 JS basics exercises added

---
+++ lesson-01/1h.js
@@ -0,0 +1,5 @@
+let tshirtPrice = 10;
+let socksPrice = 8;
+let dinnerplatesPrice = 20;
+let totalPrice = tshirtPrice + socksPrice + dinnerplatesPrice;
+console.log(`$${totalPrice}`);
---
+++ lesson-01/1i.js
@@ -0,0 +1,6 @@
+let balance = 100;
+let launchPrice = 20;
+let dinnerPrice = 50;
+let income = 200;
+let total = balance - launchPrice - dinnerPrice + income;
+console.log(`$${total}`);
