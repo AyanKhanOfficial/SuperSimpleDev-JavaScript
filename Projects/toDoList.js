fc19ddf3e14ef463b7b8049a3140cfbdb3f2e002
AyanKhanOfficial <muhammad.swat881@gmail.com>
2026-02-15 14:17:44 +0500

    Lesson-11 Loops and Arrays Added

--- Projects/toDoList.js
+++ Projects/toDoList.js
@@ -1,10 +1,19 @@
 const toDoList = [];
-function addToDo(){
-  const inputElement = document.querySelector('.js-input-field');
-  const name = inputElement.value;
- toDoList.push(name);
- inputElement.value = '';
- 
- console.log(toDoList);
 
+function addToDo() {
+  const inputElement = document.querySelector(".js-input-field");
+  const name = inputElement.value;
+  toDoList.push(name);
+  inputElement.value = "";
+  renderTodoList();
+}
+function renderTodoList() {
+  const todoContainer = document.querySelector(".js-todo-list");
+  todoContainer.innerHTML = ""; // clear old list
+
+  for (let i = 0; i < toDoList.length; i++) {
+    const p = document.createElement("p");
+    p.innerText = toDoList[i];
+    todoContainer.appendChild(p);
+  }
 }
